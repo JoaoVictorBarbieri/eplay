b4e378dc6d2b43bc32aa911a8589b18b927a3eeb
Joaquinn <joaov.b.euwest@gmail.com>
2025-04-16 17:26:41 -0300

    #Categories & Routes

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,46 +1,27 @@
+import Game from '../../models/Game'
 import Product from '../Product'
 import { Container, List } from './style'
 export type Props = {
   title: string
+  games: Game[]
   background: 'gray' | 'black'
 }
-const ProductsList = ({ background, title }: Props) => (
+const ProductsList = ({ background, title, games }: Props) => (
   <Container background={background}>
     <div className="container">
       <h2>{title}</h2>
       <List>
-        <Product
-          category="Ação"
-          description="Teste"
-          image="//place-hold.it/222x250"
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Nome do jogo"
-        />
-        <Product
-          category="Ação"
-          description="Teste"
-          image="//place-hold.it/222x250"
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Nome do jogo"
-        />
-        <Product
-          category="Ação"
-          description="Teste"
-          image="//place-hold.it/222x250"
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Nome do jogo"
-        />
-        <Product
-          category="Ação"
-          description="Teste"
-          image="//place-hold.it/222x250"
-          infos={['-10%', 'R$ 150']}
-          system="Windows"
-          title="Nome do jogo"
-        />
+        {games.map((game) => (
+          <Product
+            key={game.id}
+            category={game.category}
+            description={game.description}
+            image={game.image}
+            infos={game.infos}
+            system={game.system}
+            title={game.title}
+          />
+        ))}
       </List>
     </div>
   </Container>
